52b9fb45c242714fa37d49f0c28babeb3ced0bf9
gimbyeongjin <kbj2043@gmail.com>
2020-01-11 21:01:57 +0900

    eslint 파일 적용, login 파일 추가

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "airbnb-base"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2019,
+        "sourceType": "module"
+    },
+    "rules": {
+        "no-unused-vars": ["error", { "argsIgnorePattern": "next" }],
+        "no-console": "off"
+    }
+};
--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var stocksRouter = require('./routes/stocks');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const stocksRouter = require('./routes/stocks');
+const loginRouter = require('./routes/login');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -18,5 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/stocks', stocksRouter);
+app.use('/login', loginRouter);
 
 module.exports = app;
