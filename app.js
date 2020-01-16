394443922118a61a0e17509410635b706a708147
gimbyeongjin <kbj2043@gmail.com>
2020-01-17 02:24:10 +0900

    cors 적용한 부분 수정

--- app.js
+++ app.js
@@ -43,6 +43,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(basePath, indexRouter);
-app.use(cors);
+app.use(cors());
 
 module.exports = app;
