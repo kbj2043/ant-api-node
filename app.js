ef0baad19a63884af48f7e56a4d62275bd4d55b9
lukas.kbj <lukas.kbj@kakaocorp.com>
2020-01-26 12:48:34 +0900

    temp

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const indexRouter = require('./routes/index');
 
 const apiVersion = '1.0.0';
@@ -14,33 +13,14 @@ const basePath = `/api/v${majorVersion}/`;
 // Swagger definition
 // You can set every attribute except paths and swagger
 // https://github.com/swagger-api/swagger-spec/blob/master/versions/2.0.md
-const swaggerDefinition = {
-  info: { // API informations (required)
-    title: 'Ant API Service', // Title (required)
-    version: apiVersion, // Version (required)
-    description: 'Ant 웹서비스를 위한 API 문서', // Description (optional)
-  },
-  host: 'localhost:3000', // Host (optional)
-  basePath, // Base path (optional)
-};
-// Options for the swagger docs
-const options = {
-  // Import swaggerDefinitions
-  swaggerDefinition,
-  // Path to the API docs
-  apis: ['./routes/controllers/*.js'],
-};
-// Initialize swagger-jsdoc -> returns validated swagger spec in json format
-const swaggerSpec = swaggerJSDoc(options);
 
 const app = express();
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(basePath, indexRouter);
 
 module.exports = app;
